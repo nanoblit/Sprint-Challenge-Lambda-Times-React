e60306f3e72654f85a9e95b38e79388c5b3876a2
nanoblit <nanoblit@users.noreply.github.com>
2019-06-07 11:29:56 +0200

    Make carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,50 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import { carouselData } from '../../data';
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
-    }
+      images: [],
+      currentIndex: 0,
+    };
   }
-  componentDidMount(){
 
+  componentDidMount() {
+    this.setState({ images: carouselData });
   }
 
   leftClick = () => {
-
-  }
+    this.setState(prevState => {
+      let currentIndex = prevState.currentIndex + 1;
+      if (currentIndex >= prevState.images.length) {
+        currentIndex = 0;
+      }
+      return { currentIndex };
+    });
+  };
 
   rightClick = () => {
+    this.setState(prevState => {
+      let currentIndex = prevState.currentIndex - 1;
+      if (currentIndex < 0) {
+        currentIndex = prevState.images.length - 1;
+      }
+      return { currentIndex };
+    });
+  };
 
-  }
-
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+  render() {
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <div className="left-button" onClick={this.leftClick}>
+          {'<'}
+        </div>
+        <img src={this.state.images[this.state.currentIndex]} style={{ display: 'block' }} />
+        <div className="right-button" onClick={this.rightClick}>
+          {'>'}
+        </div>
       </div>
-    )
+    );
   }
 }
