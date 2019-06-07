a8ea7bf3e1ff4157b981db15edee42670606c9dd
nanoblit <nanoblit@users.noreply.github.com>
2019-06-07 10:07:21 +0200

    Finish tabs and cards

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Card = props => {
-  return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
-        </div>
-        <span>By {/* author goes here */}</span>
+const Card = ({ card }) => (
+  <div className="card">
+    <div className="headline">{card.headline}</div>
+    <div className="author">
+      <div className="img-container">
+        <img src={card.img} />
       </div>
+      <span>By {card.author}</span>
     </div>
-  );
-};
+  </div>
+);
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+  card: PropTypes.shape({
+    headline: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Card;
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
-}
+const Cards = ({ cards }) => (
+  <div className="cards-container">
+    {cards.map((card, idx) => (
+      <Card key={idx} card={card} />
+    )) /* Using the cards prop, map over the list creating a
+          new Card component for each passing the card as the only prop */}
+  </div>
+);
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      headline: PropTypes.string.isRequired,
+      img: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+Cards.defaultProps = {
+  cards: [],
+};
 
 export default Cards;
